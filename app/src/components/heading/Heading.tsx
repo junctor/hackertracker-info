a810196a3713338db1a227bd20bb6a31658c368f
cak <cak@fastmail.com>
2022-06-21 06:33:21 -0400

    adjust navbar h

--- app/src/components/heading/Heading.tsx
+++ app/src/components/heading/Heading.tsx
@@ -3,7 +3,7 @@ import { SearchIcon } from "@heroicons/react/outline";
 
 export function Heading() {
   return (
-    <div className='navbar bg-black sticky top-0 z-50'>
+    <div className='navbar bg-black sticky top-0 z-50 h-16'>
       <div className='navbar-start'>
         <div className='dropdown'>
           <NavLinks />

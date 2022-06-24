fb436195c07be7a82c7b31d43405b295d6688b3c
cak <cak@fastmail.com>
2022-06-23 21:35:04 -0400

    add custom 404

---
+++ app/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import Heading from "../components/heading/Heading";
+
+export default function FourOhFour({ title }: { title: string | null }) {
+  return (
+    <div>
+      <Heading />
+      <h1>404 - {title ? title : "Page Not Found"}</h1>;
+    </div>
+  );
+}

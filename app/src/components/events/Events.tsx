adcd86386ee55ab7a71e6d7e0409717786654ec2
cak <cak@fastmail.com>
2022-06-30 06:33:02 -0400

    add day sticky header

--- app/src/components/events/Events.tsx
+++ app/src/components/events/Events.tsx
@@ -4,15 +4,31 @@ import EventCell from "./EventCell";
 import Theme from "../../utils/theme";
 import { getBookmarks } from "../../utils/storage";
 
-export const Schedule = ({ dateGroup }: EventsProps) => {
-  const [bookmarks, setBookmarks] = useState<string[]>([]);
-
+export const Events = ({ dateGroup }: EventsProps) => {
   const componentRef = useRef<HTMLDivElement>(null);
   const theme = new Theme();
 
+  const bookmarks = getBookmarks();
+
   useEffect(() => {
-    setBookmarks(getBookmarks());
-  }, []);
+    if (!componentRef.current) return;
+
+    document.querySelectorAll(".event-days").forEach((eE) => {
+      let element = eE as HTMLDivElement;
+      const observer = new IntersectionObserver(
+        ([entry]) =>
+          element.classList.toggle("invisible", entry.isIntersecting),
+        { root: null, rootMargin: "-180px 0px 0px 0px", threshold: 0.25 }
+      );
+      observer.observe(eE);
+
+      return () => {
+        observer.disconnect();
+      };
+    });
+  }, [dateGroup]);
+
+  const isBookMarked = (id: string) => bookmarks?.some((b) => b === id);
 
   const scrollToDay = (day: string) => {
     if (componentRef && componentRef.current) {
@@ -34,7 +50,10 @@ export const Schedule = ({ dateGroup }: EventsProps) => {
     <div>
       {htEvents.map((htEvent) => (
         <div key={htEvent.id} id={htEvent.id.toString()}>
-          <EventCell event={htEvent} bookmarks={bookmarks} />
+          <EventCell
+            event={htEvent}
+            bookmarked={isBookMarked(htEvent.id.toString())}
+          />
         </div>
       ))}
     </div>
@@ -52,12 +71,14 @@ export const Schedule = ({ dateGroup }: EventsProps) => {
             className='scroll-m-20'>
             <div
               id={divDay(day)}
-              className='bg-black sticky top-20 z-20 mb-5 event-days'>
+              className='bg-black sticky top-20 z-20 mb-5 event-days invisible'>
               <div className='tabs tabs-boxed bg-black justify-center'>
                 {Array.from(dateGroup).map(([tabDay]) => (
                   <button
                     key={tabDay}
-                    className={`btn md:btn-sm btn-xs text-${theme.nextColor} ${
+                    className={`btn md:btn-sm btn-xs group-active:btn-active text-${
+                      theme.nextColor
+                    } ${
                       day == tabDay ? "btn-active btn-secondary" : "btn-ghost"
                     }`}
                     onClick={() => scrollToDay(tabDay)}>
@@ -66,6 +87,9 @@ export const Schedule = ({ dateGroup }: EventsProps) => {
                 ))}
               </div>
             </div>
+            <p className='text-white text-2xl ml-3 mt-3 text-center'>
+              {dateGroupTitle(day)}
+            </p>
             <div>
               <EventDisplay htEvents={htEvents} />
             </div>
@@ -76,4 +100,4 @@ export const Schedule = ({ dateGroup }: EventsProps) => {
   );
 };
 
-export default memo(Schedule);
+export default memo(Events);

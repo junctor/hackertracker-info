50061eae60ec84389625a883cb83fe951d8228ae
cak <cak@fastmail.com>
2022-07-03 06:23:35 -0400

    separate header from event details

--- app/src/components/events/Event.tsx
+++ app/src/components/events/Event.tsx
@@ -1,54 +1,16 @@
+import { EventDetailHeading } from "../heading/EventDetailsHeading";
 import NavLinks from "../heading/NavLinks";
 import EventDetails from "./EventDetails";
-import { StarIcon as StarIconOutline } from "@heroicons/react/outline";
-import { StarIcon as StarIconSoild } from "@heroicons/react/solid";
-import { useEffect, useState } from "react";
-import { addBookmark, getBookmarks, removeBookmark } from "../../utils/storage";
 
 export const Event = ({ event }: EventDetailProps) => {
-  const [bookmark, setBookmark] = useState(false);
-
-  useEffect(() => {
-    const bookmarks = getBookmarks();
-    setBookmark(bookmarks?.some((b) => b === event.id.toString()));
-  }, [event]);
-
-  const eventBookmark = () => {
-    console.log(bookmark);
-    if (!bookmark) {
-      setBookmark(true);
-      addBookmark(event.id.toString());
-    } else {
-      setBookmark(false);
-      removeBookmark(event.id.toString());
-    }
-  };
-
   return (
     <div>
-      <div className='navbar bg-black sticky top-0 z-50 h-16'>
-        <div className='navbar-start'>
-          <div className='dropdown'>
-            <NavLinks />
-          </div>
-        </div>
-        <div className='navbar-center'>
-          <p className='md:text-5xl lg:text-5xl text-4xl text-white font-bold font-mono'>
-            D<span className='text-dc-red'>3</span>F C
-            <span className='text-dc-red'>0</span>N
-          </p>
-        </div>
-        <div className='navbar-end'>
-          <div onClick={() => eventBookmark()}>
-            {bookmark ? (
-              <StarIconSoild className='w-7 mr-3' />
-            ) : (
-              <StarIconOutline className='w-7 mr-3' />
-            )}
-          </div>
-        </div>
-      </div>
-      {event && <EventDetails event={event} />}
+      {event && (
+        <>
+          <EventDetailHeading eventId={event.id} />
+          <EventDetails event={event} />
+        </>
+      )}{" "}
     </div>
   );
 };
---
+++ app/src/components/heading/EventDetailsHeading.tsx
@@ -0,0 +1,56 @@
+import NavLinks from "./NavLinks";
+import { StarIcon as StarIconOutline } from "@heroicons/react/outline";
+import { StarIcon as StarIconSoild } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
+import { addBookmark, getBookmarks, removeBookmark } from "../../utils/storage";
+
+export const EventDetailHeading = ({ eventId }: EventDetailHeaderProps) => {
+  const [bookmark, setBookmark] = useState(false);
+
+  useEffect(() => {
+    const bookmarks = getBookmarks();
+    setBookmark(bookmarks?.some((b) => b === eventId.toString()));
+  }, [eventId]);
+
+  const eventBookmark = () => {
+    console.log(bookmark);
+    if (!bookmark) {
+      setBookmark(true);
+      addBookmark(eventId.toString());
+    } else {
+      setBookmark(false);
+      removeBookmark(eventId.toString());
+    }
+  };
+
+  return (
+    <header className='sticky top-0 z-50 '>
+      <nav className='flex bg-black h-20 items-center justify-around pt-2'>
+        <div className='flex-none ml-1'>
+          <div>
+            <NavLinks />
+          </div>
+        </div>
+        <div className='flex-1 my-auto'>
+          <div className='text-center'>
+            <h1 className='text-xl sm:text-4xl md:text-5xl lg:text-6xl font-bold font-mono'>
+              D<span className='text-dc-red'>3</span>F C
+              <span className='text-dc-red'>0</span>N
+            </h1>
+          </div>
+        </div>
+        <div className='flex text-right mr-5'>
+          <div onClick={() => eventBookmark()}>
+            {bookmark ? (
+              <StarIconSoild className='w-7 mr-3' />
+            ) : (
+              <StarIconOutline className='w-7 mr-3' />
+            )}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default EventDetailHeading;

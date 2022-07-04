612123fd34b9c96ba04dc87ef83a87e625c29e2c
cak <cak@fastmail.com>
2022-07-04 06:03:16 -0400

    add bookmarks page

---
+++ app/src/components/events/Bookmarks.tsx
@@ -0,0 +1,43 @@
+import { memo, useEffect, useMemo, useRef, useState } from "react";
+import { createDateGroup, groupedDates } from "../../utils/dates";
+import NavLinks from "../heading/NavLinks";
+import { SearchIcon } from "@heroicons/react/outline";
+import Events from "./Events";
+import Loading from "../misc/Loading";
+import EventHeading from "../heading/EventHeading";
+import { getBookmarks } from "../../utils/storage";
+
+export const Bookmarks = ({ events, title }: ScheduleProps) => {
+  let [bookmarkedEvents, setBookmarkedEvents] = useState<EventData[]>([]);
+
+  useEffect(() => {
+    const bookmarks = getBookmarks();
+    const bEvents = events.filter((e) => bookmarks.includes(e.id));
+    setBookmarkedEvents(bEvents);
+  }, [events]);
+
+  let dateGroup = createDateGroup(bookmarkedEvents);
+
+  let headingEvents: EventSearch[] = bookmarkedEvents
+    .map((e) => ({
+      title: e.title,
+      id: e.id,
+      color: e.color,
+    }))
+    .sort((a, b) => {
+      if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+  return (
+    <div>
+      <EventHeading events={headingEvents} />
+      <Events dateGroup={dateGroup} title={title} />
+    </div>
+  );
+};
+
+export default Bookmarks;
---
+++ app/src/pages/events/bookmarks.tsx
@@ -0,0 +1,50 @@
+import type { NextPage } from "next";
+import { promises as fs } from "fs";
+import path from "path";
+import Head from "next/head";
+import { toEventsData } from "../../utils/misc";
+import Bookmarks from "../../components/events/Bookmarks";
+
+const BookmarksPage: NextPage<ScheduleProps> = (props) => {
+  const { events } = props;
+  return (
+    <div>
+      <Head>
+        <title>D3F C0N Bookmarks</title>
+        <meta name='description' content='DEF CON 30' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className='bg-black'>
+        <Bookmarks events={events} title={"Bookmarks"} />
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const fileToRead = path.join(
+    process.cwd(),
+    "./public/static/conf/events.json"
+  );
+
+  let eventFile = await fs.readFile(fileToRead, {
+    encoding: "utf-8",
+  });
+
+  let events: HTEvent[] = JSON.parse(eventFile) ?? [];
+
+  let eventsData = toEventsData(events);
+
+  return {
+    props: {
+      events: eventsData,
+    },
+  };
+}
+
+export default BookmarksPage;

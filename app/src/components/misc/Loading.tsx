03c6c8e3c6dceb667d3e9c0495a05f32c80516c4
cak <cak@fastmail.com>
2022-07-07 06:07:48 -0400

    loading spinner

--- app/src/components/misc/Loading.tsx
+++ app/src/components/misc/Loading.tsx
@@ -5,7 +5,7 @@ export function Loading() {
       <img
         src='/static/img/skull_200x200.png'
         alt='DEF CON Logo'
-        className='animate-pulse m-auto w-14 sm:w-16 md:w-20 lg:w-24 block'
+        className='animate-spin m-auto w-14 sm:w-16 md:w-20 lg:w-24 block'
       />
     </div>
   );

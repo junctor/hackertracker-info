ee1e402c8af27fb46cb164edb3b744ded2e8cdc1
cak <cak@fastmail.com>
2022-07-23 05:51:09 -0400

    adjust countdown date

--- app/src/components/countdown/Countdown.tsx
+++ app/src/components/countdown/Countdown.tsx
@@ -11,7 +11,7 @@ function Countdown() {
 
   function getCountdown(): Timer {
     const now = new Date().valueOf();
-    const dc30 = new Date("2022-08-11T00:00:00.000-07:00").valueOf();
+    const dc30 = new Date("2022-08-12T17:00:00+00:00").valueOf();
     const d = (dc30 - now) / (24 * 60 * 60 * 1000);
     const h = (d % 1) * 24;
     const m = (h % 1) * 60;

1a92f14a4e8264bbdb30655d67645691c78a9807
cak <cak@fastmail.com>
2022-07-24 06:56:54 -0400

    remove embedded maps

--- app/src/components/maps/MapsDisplay.tsx
+++ app/src/components/maps/MapsDisplay.tsx
@@ -9,37 +9,21 @@ function MapsDisplay({ conference }: MapProps) {
   }));
 
   return (
-    <div className='mt-10 w-full px-10'>
-      <Tab.Group>
-        <Tab.List className='my-2 justify-center items-center flex'>
-          {confMaps.map((m) => (
-            <Tab key={m.name}>
-              {({ selected }: { selected: boolean }) => (
-                <button
-                  type='button'
-                  className={`
-                  p-2 mx-1 rounded-lg text-xs sm:text-sm md:text-base lg:text-lg font-semibold text-white
-                    ${selected ? "bg-dc-blue" : "hover:text-gray-400"}
-                  `}>
-                  {m.name}
-                </button>
-              )}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels>
-          {confMaps.map((m) => (
-            <Tab.Panel key={m.name}>
-              <embed
-                src={`/static/conf/maps/${m.map}`}
-                width='100%'
-                height='800px'
-                type='application/pdf'
-              />
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
+    <div className='mt-10 w-full px-10 items-center align-middle text-center'>
+      <div>
+        {confMaps.map((m) => (
+          <div
+            key={m.map}
+            className='sm:inline p-2 mx-1 rounded-lg text-sm sm:text-base md:text-lg lg:text-xl font-bold hover:text-dc-pink'>
+            <a
+              href={`/static/conf/maps/${m.map}`}
+              target='_blank'
+              rel='noopener noreferrer'>
+              {m.name}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

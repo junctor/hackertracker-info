4e1dc09aca0931ab5b19b4d4a9d30b3119e92907
cak <cak@fastmail.com>
2022-07-26 04:32:59 -0400

    update safe colors

--- app/tailwind-ht-colors.sh
+++ app/tailwind-ht-colors.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-COLORS=$(jq '.[].type.color' ./public/static/conf/events.json | sort -u)
-jq -r -c --argjson colors $COLORS '.colors = [$colors]' ./tailwind-ht-safelist.json > colors.tmp && mv colors.tmp ./tailwind-ht-safelist.json
+COLORS=$(jq '.[].type.color' ./public/static/conf/events.json | sort -u | tr '\n' ',' | sed 's/.$//')
+COLORS=["$COLORS"]
+jq -r -c --argjson colors "$COLORS" '.colors = [$colors]' ./tailwind-ht-safelist.json > c.tmp && mv c.tmp ./tailwind-ht-safelist.json

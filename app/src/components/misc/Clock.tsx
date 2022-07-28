effcb3f13f38abe768c8911cc9eb98020360d8ab
cak <cak@fastmail.com>
2022-07-27 20:10:40 -0400

    clock updates

--- app/src/components/misc/Clock.tsx
+++ app/src/components/misc/Clock.tsx
@@ -16,11 +16,27 @@ function Clock() {
   }, []);
 
   return (
-    <span className='countdown text-lg sm:text-xl md:text-2xl lg:text-3xl'>
-      <span style={{ "--value": time.getHours() } as React.CSSProperties} />:
-      <span style={{ "--value": time.getMinutes() } as React.CSSProperties} />:
-      <span style={{ "--value": time.getSeconds() } as React.CSSProperties} />
-    </span>
+    <div className='flex items-center'>
+      <span className='countdown flex text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold'>
+        <span
+          className='text-dc-pink'
+          style={{ "--value": time.getHours() } as React.CSSProperties}
+        />
+        :
+        <span
+          className='text-dc-blue'
+          style={{ "--value": time.getMinutes() } as React.CSSProperties}
+        />
+        :
+        <span
+          className='text-dc-green'
+          style={{ "--value": time.getSeconds() } as React.CSSProperties}
+        />
+      </span>
+      <p className='flex ml-1 md:ml-2 lg:ml-3 text-xs sm:text-sm md:text-base lg:text-lg'>
+        PDT
+      </p>
+    </div>
   );
 }
 

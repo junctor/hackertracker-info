5cab5d1ef79ec00da45d8359217b7a07a11960e3
cak <cak@fastmail.com>
2022-07-31 04:58:22 -0400

    category cell styling (word breaks)

--- app/src/components/categories/CategoryCell.tsx
+++ app/src/components/categories/CategoryCell.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 function CategoryCell({ category }: CategoryCellProps) {
   return (
     <div className='my-5 ml-2'>
-      <div className='flex bg-black items-center text-left'>
+      <div className='table bg-black items-center text-left'>
         <div
-          className={`ml-1 w-1 md:w-2 h-7 sm:h-8 md:h-9 lg:h-10 ${`bg-[${category.color}]`}`}>
+          className={`table-cell ml-1 w-1 md:w-2 ${`bg-[${category.color}]`}`}>
           &nbsp;
         </div>
-        <div className='w-11/12 ml-2'>
+        <div className='ml-2 w-full'>
           <Link href={`/categories/${category.id}`} prefetch={false}>
             <button
               type='button'
-              className='text-base sm:text-lg md:text-xl lg:text-2xl font-bold'>
+              className='text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-left'>
               {category.name}
             </button>
           </Link>

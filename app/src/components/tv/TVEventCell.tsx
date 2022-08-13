fb547499c8ea77c7269713b0250cfb9278549d39
cak <cak@fastmail.com>
2022-08-13 07:56:40 -0700

    margin on tv cell

--- app/src/components/tv/TVEventCell.tsx
+++ app/src/components/tv/TVEventCell.tsx
@@ -7,23 +7,25 @@ function TVEventCell({ event }: TVEventProps) {
         <div
           className={`table-cell px-2 h-full w-3 bg-[${event.color}] rounded-md`}
         />
-        <div className='w-full table-cell'>
-          <h1 className='text-3xl font-bold text-left break-words'>
-            {event.title}
-          </h1>
+        <div className='ml-5'>
+          <div className='w-full table-cell'>
+            <h1 className='text-3xl font-bold text-left break-words'>
+              {event.title}
+            </h1>
 
-          <p className='text-lg'>{event.speakers}</p>
+            <p className='text-lg'>{event.speakers}</p>
 
-          <p className='text-xl text-gray-400'>{event.location}</p>
-          <div
-            className={`rounded-full h-3 w-3 inline-flex mr-2 bg-[${event.color}]`}
-          />
-          <p className='text-base inline-flex'>{event.category}</p>
-          <p className='text-xl text-dc-green font-bold'>
-            {`${eventTime(new Date(event.begin), false)} - ${eventTime(
-              new Date(event.end)
-            )}`}
-          </p>
+            <p className='text-xl text-gray-400'>{event.location}</p>
+            <div
+              className={`rounded-full h-3 w-3 inline-flex mr-2 bg-[${event.color}]`}
+            />
+            <p className='text-base inline-flex'>{event.category}</p>
+            <p className='text-xl text-dc-green font-bold'>
+              {`${eventTime(new Date(event.begin), false)} - ${eventTime(
+                new Date(event.end)
+              )}`}
+            </p>
+          </div>
         </div>
       </div>
     </div>

3140b008ccd32ed0d4e74f9d3476aeb0a20c5aeb
cak <cak@fastmail.com>
2023-08-08 06:27:14 -0400

    fix issue with creating /ht-exports folder

--- app/ht-build.sh
+++ app/ht-build.sh
@@ -1,5 +1,5 @@
-rm -rf ../ht-exports/out
-mkdir -p ../ht-exports/out
+rm -rf ../ht-export/out
+mkdir -p ../ht-export/out
 npm run --prefix ../ht-export export
 cp -r ../ht-export/out/ht/ public/ht
 bash ./tailwind-ht-colors.sh

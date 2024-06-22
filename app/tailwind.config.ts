ebaf6875b7160f3c73660971c444a37effdfc9c8
cak <cak@fastmail.com>
2024-06-22 05:57:49 -0400

    update configs

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,32 +1,104 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const htSafeList = require("./tailwind-ht-safelist.json");
+
+const config = {
+  darkMode: "class",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  safelist: [
+    // DC30 Theme
+    "text-dc-purple",
+    "text-dc-teal",
+    "text-dc-yellow",
+    "text-dc-red",
+    "bg-dc-purple",
+    "bg-dc-teal",
+    "bg-dc-yellow",
+    "bg-dc-red",
+    //  Event colors
+    ...htSafeList.colors.map((c: string) => `bg-[${c}]`),
+    ...htSafeList.colors.map((c: string) => `hover:text-[${c}]`),
+    ...htSafeList.colors.map((c: string) => `hover:bg-[${c}]`),
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        "dc-purple": "#892BE1",
-        "dc-teal": "#0A7985",
-        "dc-yellow": "#FFAC11",
-        "dc-red": "#DB1675",
+        "dc-purple": "#686EA0",
+        "dc-teal": "#81C8BD",
+        "dc-yellow": "#ECDA25",
+        "dc-red": "#F8A28B",
         "dc-text": "#eeeeee",
         "dc-gray": "#2D2D2D",
         "dc-border": "#646464",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: [],
-  },
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;

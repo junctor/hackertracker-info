6c45cfdad83c4be0d82d267e995a66222d570a60
cak <cak@fastmail.com>
2024-06-24 05:29:37 -0400

    change orgs to table

--- app/src/components/organization/Organizations.tsx
+++ app/src/components/organization/Organizations.tsx
@@ -1,5 +1,15 @@
-import OrgCell from "./OrgCell";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useRouter } from "next/router";
 
 export default function Orgs({
   orgs,
@@ -8,19 +18,36 @@ export default function Orgs({
   orgs: HTOrganization[];
   title: string;
 }) {
+  const router = useRouter();
+
   return (
     <>
       <div className="mx-5 bg-background">
-        <div className="my-10">
-          <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-extrabold font-mono">
-            {title}
-          </h1>
-        </div>
-        {orgs.map((o) => (
-          <div key={o.id} className="my-5">
-            {o.id != null && <OrgCell org={o} />}
-          </div>
-        ))}
+        <Table>
+          <TableCaption>{title}</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-20"></TableHead>
+              <TableHead>{title}</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orgs.map((o) => (
+              <TableRow
+                key={o.id}
+                onClick={() => router.push(`/org/?id=${o.id}`)}
+              >
+                <TableCell>
+                  <Avatar>
+                    <AvatarImage src={o.logo.url} />
+                    <AvatarFallback>{o.name[0]}</AvatarFallback>
+                  </Avatar>
+                </TableCell>
+                <TableCell>{o.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </>
   );

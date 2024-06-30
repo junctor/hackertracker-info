a381ad138fcf8cb4e332271e63333a74b8150b3c
cak <cak@fastmail.com>
2024-06-30 06:25:37 -0400

    add page: event

---
+++ app/src/components/event/Event.tsx
@@ -0,0 +1,172 @@
+import {
+  ClockIcon,
+  MapIcon,
+  CalendarDaysIcon,
+  ArrowUpOnSquareIcon,
+} from "@heroicons/react/24/outline";
+import cal from "../../lib/utils/cal";
+import { eventTime } from "../../lib/utils/dates";
+import ReactMarkdown from "react-markdown";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { BASEURL } from "@/lib/utils/const";
+import React from "react";
+
+function Event({ event }: { event: HTEvent }) {
+  return (
+    <div className="mx-5">
+      <div className="flex">
+        <div className="mt-2 ml-5 mb-5 justify-start flex-auto">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="../events">Schedule</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div className="my-3">
+            <h1
+              className={`font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl mb-5 mr-3 text-[${event.type.color}]`}
+            >
+              {event.title}
+            </h1>
+          </div>
+        </div>
+        <div className="mr-10 ml-5 content-center justify-end flex-none">
+          <div className="flex">
+            <a
+              href={`data:text/calendar;charset=utf8,${encodeURIComponent(
+                cal(event)
+              )}`}
+              download={`${event.conference}-${event.id}.ics`}
+            >
+              <CalendarDaysIcon className="h-5 w-5 md:h-7 md:w-7 lg:w-8 lg:h-8 mr-2" />
+            </a>
+            {typeof navigator.share === "function" && (
+              <ArrowUpOnSquareIcon
+                className="h-5 w-5 md:h-7 md:w-7 lg:w-8 lg:h-8 mr-2"
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                onClick={async (): Promise<void> => {
+                  try {
+                    await navigator.share({
+                      title: event.title,
+                      url: `${BASEURL}/event/?id=${event.id}`,
+                    });
+                  } catch (e) {
+                    return;
+                  }
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="font-bold">
+        <div className="flex items-center w-11/12 my-2 cursor-pointer mx-2">
+          <a
+            className="flex"
+            href={`data:text/calendar;charset=utf8,${encodeURIComponent(
+              cal(event)
+            )}`}
+            download={`${event.conference}-${event.id}.ics`}
+          >
+            <ClockIcon className="h-5 w-5 md:h-7 md:w-7 lg:w-8 lg:h-8 mr-2" />
+            <p className="md:text-base lg:text-lg text-xs">
+              {event.end_timestamp.seconds !== event.begin_timestamp.seconds
+                ? `${eventTime(new Date(event.begin), false)} - ${eventTime(
+                    new Date(event.end),
+                    true
+                  )}`
+                : `${eventTime(new Date(event.begin), true)}`}
+            </p>
+          </a>
+        </div>
+        <div className="flex items-center w-11/12 my-2 mx-2">
+          <MapIcon className="h-5 w-5 md:h-7 md:w-7 lg:w-8 lg:h-8 mr-2" />
+          <p className="md:text-base lg:text-lg text-xs">
+            {event.location.name}
+          </p>
+        </div>
+        <div className="flex items-center w-11/12 my-2 mx-2">
+          <span
+            className={`rounded-full h-4 w-4 md:h-5 md:w-5 lg:w-6 lg:h-6 mr-2 green inline-flex flex-none bg-[${event.type.color}]`}
+          />
+          <p className={`text-xs md:text-sm lg:text-base`}>{event.type.name}</p>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <div className="text-sm md:text-base lg:text-lg w-11/12">
+          <div className="prose lg:prose-xl whitespace-pre-wrap">
+            <ReactMarkdown>{event.description}</ReactMarkdown>
+          </div>
+          {(event.links ?? []).length > 0 && (
+            <div className="mt-5 text-left">
+              <h2 className="font-bold text-base sm:text-lg md:text-xl lg:text-2xl">
+                Links
+              </h2>
+              <ul className="list-disc text-xs sm:text-sm md:text-base lg:text-lg ml-5 mt-2">
+                {(event.links ?? []).map((l) => (
+                  <li key={l.url}>
+                    <a href={l.url}>{l.label}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      {event.speakers.length > 0 && (
+        <div className="mt-10 text-left">
+          <h2 className="font-bold text-base sm:text-lg md:text-xl lg:text-2xl">
+            Speakers
+          </h2>
+          <div className="items-center w-11/12 mt-1 mb-2 pt-2 pb-2">
+            {event.speakers
+              .sort(
+                (a) =>
+                  (findSortOrder(event.people, a.id) ?? 0) -
+                  (findSortOrder(event.people, a.id) ?? 0)
+              )
+              .map((s) => (
+                <div
+                  key={s.id}
+                  className="ml-3 table mt-2 mb-2 align-middle items-center"
+                >
+                  <div
+                    className={`ml-1 table-cell h-full w-1 sm:w-2 mr-3 rounded-md`}
+                  />
+                  <div className="inline-block text-left ml-2">
+                    <p className="font-bold text-sm sm:text-md md:text-base lg:text-lg">
+                      {s.name}
+                    </p>
+
+                    {s.title != null && (
+                      <p className="text-xs sm:text-xs md:text-xs lg:text-sm">
+                        {s.title}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function findSortOrder(people: HTPeople[], id: number) {
+  return people.find((p) => p.person_id === id)?.sort_order;
+}
+
+export default Event;
---
+++ app/src/pages/event/index.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Event from "../../components/event/Event";
+import useSWR from "swr";
+import { fetcher } from "../../lib/utils/misc";
+import Loading from "@/components/misc/Loading";
+import Error from "@/components/misc/Error";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import Heading from "@/components/heading/Heading";
+
+export default function EventPage() {
+  const searchParams = useSearchParams();
+
+  const {
+    data: eventsData,
+    error: eventsError,
+    isLoading: eventsIsLoading,
+  } = useSWR<HTEvent[], Error>(`../../../ht/events.json`, fetcher);
+
+  if (eventsIsLoading) {
+    return <Loading />;
+  }
+
+  if (eventsData === undefined || eventsError !== undefined) {
+    return <Error msg={eventsError?.message} />;
+  }
+
+  const eventId = searchParams.get("id");
+
+  if (eventId == null || eventId === "") {
+    return <Error msg="No event id provided" />;
+  }
+
+  const event = eventsData?.find(
+    (e) => e.id.toString().toLowerCase() === eventId.toLowerCase()
+  );
+
+  if (event === undefined) {
+    return <Error msg="No event found for id" />;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{`DC32 ${event.title}`}</title>
+        <meta name="description" content="DEF CON 32 Event" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Heading />
+        <Event event={event} />
+      </main>
+    </div>
+  );
+}

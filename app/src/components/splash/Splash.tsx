eacfa18b234f8156c440e7e2d024297b058c28b9
cak <cak@fastmail.com>
2024-08-07 06:22:40 -0400

    redirect to `/events` if DC live

--- app/src/components/splash/Splash.tsx
+++ app/src/components/splash/Splash.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Countdown from "../countdown/Countdown";
 import Image from "next/image";
 import dc32Logo from "../../../public/images/dc32-logo-2color-halftonegradient.webp";
+import { useRouter } from "next/navigation";
 
 export default function Splash() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (new Date() > new Date("2024-08-09T09:00:00-07:00")) {
+      router.push("/events");
+    }
+  }, []);
+
   return (
     <div className="my-14">
       <div className="flex w-full justify-center items-center text-center mt-5">

19a8cce8b8f995551c9ba55a228094389c8758d9
cak <cak@fastmail.com>
2024-08-08 06:26:32 -0400

    add markdown table styles

--- app/src/components/markdown/Markdown.tsx
+++ app/src/components/markdown/Markdown.tsx
@@ -1,11 +1,14 @@
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
 import React from "react";
+import styles from "../../styles/markdown.module.css";
 
 export default function Markdown({ content }: { content: string }) {
   return (
     <div className="prose lg:prose-xl whitespace-pre-wrap">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown className={styles.markdown} remarkPlugins={[remarkGfm]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }

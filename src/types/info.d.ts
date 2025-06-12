7619fbd896f8570c4f6528dd5bb98f98ce5f5f8c
cak <cak@typeerror.com>
2025-06-12 05:34:55 -0400

    move `/App` to root

---
+++ src/types/info.d.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-use-before-define */
+
+interface Timer {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+interface EventSearching {
+  event: EventSearch;
+  active: boolean;
+}
+
+interface EventSearch {
+  id: number;
+  title: string;
+  color: string;
+}
+
+interface EventData {
+  id: number;
+  begin: string;
+  beginTimestampSeconds: number;
+  endTimestampSeconds: number;
+  end: string;
+  title: string;
+  location: string;
+  color: string;
+  category: string;
+  tags: HTTags[];
+  speakers: string;
+}
+
+interface CategoryData {
+  name: string;
+  data: HTEventType | undefined;
+}
+
+interface Speaker {
+  name: string;
+  id: number;
+}

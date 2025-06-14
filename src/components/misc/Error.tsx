8991babdb9301589a43f2478248d7e0f235a90e0
cak <cak@typeerror.com>
2025-06-14 15:40:42 -0400

    Add error and loading pages

---
+++ src/components/misc/Error.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Error({ msg }: { msg?: string }) {
+  return (
+    <main>
+      <div className="flex content-center h-screen justify-center items-center text-center">
+        <Link href="/">
+          <div>
+            <h1 className=" text-2xl md:text-4xl">Error</h1>
+            <h4 className="text-xs md:text-sm">{msg ?? ""}</h4>
+            <h3 className="text-base md:text-md">Return Home</h3>
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/misc/Loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <main>
+      <div className="flex content-center h-screen">
+        <h1 className="animate-spin m-auto text-3xl block font-bold">HT</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Loading;

0b5699e72a7ed8c42d0307305ca1d9883445d332
cak <cak@typeerror.com>
2025-06-16 05:45:21 -0400

    add `Apps` page

---
+++ src/pages/apps/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Apps from "@/components/apps/Apps";
+
+export default function AppsPage() {
+  return (
+    <div>
+      <main>
+        <Apps />
+      </main>
+    </div>
+  );
+}

e2bc98738479148757ae00243da19863518b35e3
cak <cak@typeerror.com>
2025-07-31 15:18:01 -0400

    move tv to defcon-microsites

--- src/pages/tv/index.tsx
+++ src/pages/tv/index.tsx
@@ -1,36 +1,9 @@
-// src/pages/schedule/index.tsx
-import React from "react";
-import useSWR from "swr";
-import { fetcher } from "@/lib/misc";
-import Loading from "@/components/misc/Loading";
-import Error from "@/components/misc/Error";
-
-import Head from "next/head";
-import TV from "@/components/tv/TV";
-import { GroupedSchedule } from "@/types/info";
+import { useEffect } from "react";
 
 export default function TVPage() {
-  const {
-    data: schedule,
-    error,
-    isLoading,
-  } = useSWR<GroupedSchedule>("/ht/schedule.json", fetcher);
+  useEffect(() => {
+    window.location.replace("https://junctor.github.io/defcon-microsites/tv/");
+  }, []);
 
-  if (isLoading) return <Loading />;
-  if (error || !schedule) return <Error />;
-
-  return (
-    <>
-      <Head>
-        <title>TV | DEF CON</title>
-        <meta
-          name="description"
-          content="Full DEF CON 33 TV schedule of sessions, talks, and events."
-        />
-      </Head>
-      <main>
-        <TV dateGroup={schedule} />
-      </main>
-    </>
-  );
+  return null;
 }

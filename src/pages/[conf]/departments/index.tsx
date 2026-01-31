ffa268969f319fe4b0f0f2e25d6f11df8dec8b4d
cak <cak@typeerror.com>
2026-01-31 10:27:36 -0500

    refactor(DepartmentsPage): simplify component by removing unused imports and SWR logic

--- src/pages/[conf]/departments/index.tsx
+++ src/pages/[conf]/departments/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import useSWR from "swr";
-import Head from "next/head";
-import { alphaSort, fetcher } from "@/lib/misc";
-import LoadingScreen from "@/features/app-shell/LoadingScreen";
-import ErrorScreen from "@/features/app-shell/ErrorScreen";
-import SiteHeader from "@/features/app-shell/SiteHeader";
-import { Organizations as OrgsType } from "@/lib/types/info";
-import OrganizationsList from "@/features/organizations/OrganizationsList";
+import DirectoryPage from "@/features/organizations/DirectoryPage";
 import { ConferenceManifest } from "@/lib/conferences";
+import { PageId } from "@/lib/types/page-meta";
 import {
   buildConferenceStaticPaths,
   getConferenceFromParams,
 } from "@/lib/next-static";
 import type { GetStaticProps } from "next";
-import { PageId } from "@/lib/types/page-meta";
 
 type DepartmentsPageProps = {
   conf: ConferenceManifest;
@@ -24,35 +17,13 @@ export default function DepartmentsPage({
   conf,
   activePageId,
 }: DepartmentsPageProps) {
-  const {
-    data: organizations,
-    error,
-    isLoading,
-  } = useSWR<OrgsType>("/ht/organizations.json", fetcher);
-
-  if (isLoading) return <LoadingScreen />;
-  if (error || !organizations) return <ErrorScreen />;
-
-  const departments = organizations
-    .filter((org) => org.tag_ids.includes(48795))
-    .sort((a, b) => {
-      return alphaSort(a.name, b.name);
-    });
-
   return (
-    <>
-      <Head>
-        <title>Villages | DEF CON Singapore 2026</title>
-        <meta
-          name="description"
-          content="Explore all DEF CON Singapore 2026 Departments"
-        />
-      </Head>
-      <main>
-        <SiteHeader conference={conf} activePageId={activePageId} />
-        <OrganizationsList orgs={departments} title="Departments" confSlug="" />
-      </main>
-    </>
+    <DirectoryPage
+      conf={conf}
+      activePageId={activePageId}
+      title="Departments"
+      tagLabel="def_con_department"
+    />
   );
 }
 

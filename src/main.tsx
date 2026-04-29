7bed5519fd2647d7fb7a2f3cc8ca7cc75258e48f
cak <cak@typeerror.com>
2026-04-29 16:34:32 -0400

    feat: implement initial app structure with routing and lazy loading

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { SWRConfig } from "swr";
+
+import App from "./App";
+
+createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <SWRConfig
+      value={{
+        dedupingInterval: 60_000,
+        keepPreviousData: true,
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+        shouldRetryOnError: false,
+      }}
+    >
+      <App />
+    </SWRConfig>
+  </React.StrictMode>,
+);
